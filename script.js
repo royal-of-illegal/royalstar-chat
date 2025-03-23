92b0a02d91b89a247ba0b0cd493453239751f92c
royal-of-illegal <royalstar13in@gmail.com>
2025-03-23 07:35:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function sendMessage() {
     const message = document.getElementById("message").value;
     fetch("http://localhost:5000/chat/send", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ sender: "user1", receiver: "user2", message }) });
 }
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBriiZfaS2hU1qYBsyJEYs13bK-UcdrsXc",
+    authDomain: "royalstar-chat.firebaseapp.com",
+    projectId: "royalstar-chat",
+    storageBucket: "royalstar-chat.firebasestorage.app",
+    messagingSenderId: "847429163840",
+    appId: "1:847429163840:web:7d2d9ea561de6c957338a5",
+    measurementId: "G-BG7TR96RVN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
